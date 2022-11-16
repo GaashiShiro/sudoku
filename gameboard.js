8c3fe95051a87ad5e631e43342274fbfdef8db26
GaashiShiro <miknbr@gmail.com>
2022-11-16 15:58:24 +0200

    shuffle working for rows and columns

--- gameboard.js
+++ gameboard.js
@@ -12,15 +12,15 @@ class Board {
         const tile = this.tiles;
         for (let i=0; i<tile.length; i++){
             tile[i] = { index: tile.index++, value: 0, quadrant : 0, x: i % 9, y: Math.floor(i/9) }; //access the index||value in grid||quadrant||x && y of an array
-            if (tile[i].x <= 2 && tile[i].y <= 2 )                                    { tile[i].quadrant = 1 }    // Top Left Quadrant
-            if (tile[i].x >= 3 && tile[i].x <= 5 && tile[i].y <= 2 )                  { tile[i].quadrant = 2 }    // Top Middle Quadrant
-            if (tile[i].x > 5  && tile[i].y <= 2 )                                    { tile[i].quadrant = 3 }    // Top Right Quadrant
-            if (tile[i].x <= 2 && tile[i].y > 2  && tile[i].y <= 5 )                  { tile[i].quadrant = 4 }    // Center Left Quadrant
-            if (tile[i].x >= 3 && tile[i].x <= 5 && tile[i].y > 2 && tile[i].y <= 5 ) { tile[i].quadrant = 5 }    // Center Quadrant
-            if (tile[i].x > 5  && tile[i].y > 2  && tile[i].y <= 5 )                  { tile[i].quadrant = 6 }    // Center Right Quadrant
-            if (tile[i].x <= 2 && tile[i].y > 5 )                                     { tile[i].quadrant = 7 }    // Bottom Left Quadrant
-            if (tile[i].x >= 3 && tile[i].x <= 5 && tile[i].y>5 )                     { tile[i].quadrant = 8 }    // Bottom Middle Quadrant
-            if (tile[i].x > 5  && tile[i].y > 5 )                                     { tile[i].quadrant = 9 }    // Bottom Right Quadrant
+            if (tile[i].x <= 2 && tile[i].y <= 2 )                                    { tile[i].quadrant = 0 }    // Top Left Quadrant
+            if (tile[i].x >= 3 && tile[i].x <= 5 && tile[i].y <= 2 )                  { tile[i].quadrant = 1 }    // Top Middle Quadrant
+            if (tile[i].x > 5  && tile[i].y <= 2 )                                    { tile[i].quadrant = 2 }    // Top Right Quadrant
+            if (tile[i].x <= 2 && tile[i].y > 2  && tile[i].y <= 5 )                  { tile[i].quadrant = 3 }    // Center Left Quadrant
+            if (tile[i].x >= 3 && tile[i].x <= 5 && tile[i].y > 2 && tile[i].y <= 5 ) { tile[i].quadrant = 4 }    // Center Quadrant
+            if (tile[i].x > 5  && tile[i].y > 2  && tile[i].y <= 5 )                  { tile[i].quadrant = 5 }    // Center Right Quadrant
+            if (tile[i].x <= 2 && tile[i].y > 5 )                                     { tile[i].quadrant = 6 }    // Bottom Left Quadrant
+            if (tile[i].x >= 3 && tile[i].x <= 5 && tile[i].y>5 )                     { tile[i].quadrant = 7 }    // Bottom Middle Quadrant
+            if (tile[i].x > 5  && tile[i].y > 5 )                                     { tile[i].quadrant = 8 }    // Bottom Right Quadrant
         }
         console.log(this);
     }
@@ -56,18 +56,19 @@ class Board {
 
     generate(){
         let template = "387491625241568379569327418758619234123784596496253187934176852675832941812945763";
-        let randomN = randomNum();
+        let randomN1 = Math.floor(Math.random() * 10);
+        let randomN2 = Math.floor(Math.random() * 10);
         for (let i=0; i<this.tiles.length; i++){ this.tiles[i].value = template[i] }                                  // Assigns each board element with its value
         
         
-        const shuffleColumns=(times)=>{
+        const shuffleColumns=()=>{
             for (let x=0; x<9; x=x+3) {                                          //Loop for each 3 pairs
                 let col0 = this.getRow(x);
                 let col1 = this.getRow(x+1);
                 let col2 = this.getRow(x+2);
                     
-                for (let t=times; t--;){                            // Loop for number of shuffles
-                    for (let  i=0; i<9; i++){                       // Loop all results in that column        
+                for (let t=randomN1; t--;){                            // Loop for number of shuffles
+                    for (let  i=0; i<9; i++){                       // Loop all results in that column
                         [col0[i].value, col1[i].value] = [col1[i].value, col0[i].value];
                         [col1[i].value, col2[i].value] = [col2[i].value, col1[i].value];
                     }
@@ -75,13 +76,13 @@ class Board {
             }
         }
 
-        const shuffleRows=(times)=>{
+        const shuffleRows=()=>{
             for (let x=0; x<9; x=x+3) {
                 let row0 = this.getColumn(x);
                 let row1 = this.getColumn(x+1);
                 let row2 = this.getColumn(x+2);
                     
-                for (let t=times; t--;){
+                for (let t=randomN2; t--;){
                     for (let  i=0; i<9; i++){
                         [row0[i].value, row1[i].value] = [row1[i].value, row0[i].value];
                         [row1[i].value, row2[i].value] = [row2[i].value, row1[i].value];
@@ -90,26 +91,45 @@ class Board {
             }
         }
 
-        console.log(this.getQuadrant(0))
-        /*
-        const shuffleQuad=(times)=>{
-            for (let x=0; x<9; x=x+3) {
-                let row0 = this.getQuadrant(x);
-                let row1 = this.getQuadrant(x+1);
-                let row2 = this.getQuadrant(x+2);
-                    
-                for (let t=times; t--;){
-                    for (let  i=0; i<9; i++){
-                        [row0[i].value, row1[i].value] = [row1[i].value, row0[i].value];
-                        [row1[i].value, row2[i].value] = [row2[i].value, row1[i].value];
-                    }
+
+        
+        const shuffleQuadColumns=(times)=>{
+            let quad0 = this.getQuadrant(0);
+            let quad1 = this.getQuadrant(3);
+            let quad2 = this.getQuadrant(6);
+
+            let quad3 = this.getQuadrant(1);
+            let quad4 = this.getQuadrant(4);
+            let quad5 = this.getQuadrant(7);
+                
+            console.log(quad0)
+            
+            /*for (let  i=0; i<9; i++){
+                [quad0[i].value, quad1[i].value, quad2[i].value] = [quad3[i].value, quad4[i].value, quad5[i].value];
+                [quad3[i].value, quad4[i].value, quad5[i].value] = [quad0[i].value, quad1[i].value, quad2[i].value];
+
+            }*/
+            
+            
+            
+            /*
+            for (let t=times; t--;){
+                for (let  i=0; i<9; i++){
+                    [row0[i].value, row1[i].value] = [row1[i].value, row0[i].value];
+                    [row1[i].value, row2[i].value] = [row2[i].value, row1[i].value];
                 }
             }
+            */
         }
-        */
-        //shuffleColumns(randomN) 
-        //shuffleRows(randomN)
-        //shuffleQuad(randomN)
+
+
+        shuffleRows()
+        shuffleColumns()
+
+        
+
+        //shuffleQuadColumns(randomN)
+        //shuffleQuadRows(randomN)
 
         const removeRandom=(num)=>{
             let copySol = template.slice();
